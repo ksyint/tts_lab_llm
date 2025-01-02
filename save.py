c1403e46f5d6feae5787b45632b4fcce164a6687
ksyint <82583462+ksyint@users.noreply.github.com>
2025-01-02 21:11:33 +0900

    Update save.py

--- save.py
+++ save.py
@@ -6,10 +6,7 @@ save_lora="lora_checkpoint2"
 save_merge="merged"
 
 
-
-
-
-model_to_merge = PeftModel.from_pretrained(AutoModelForCausalLM.from_pretrained("Llama-3.1-Korean-8B-Instruct", device_map="auto"),\
+model_to_merge = PeftModel.from_pretrained(AutoModelForCausalLM.from_pretrained("sh2orc/Llama-3.1-Korean-8B-Instruct", device_map="auto"),\
                                             save_lora)
 
 merged_model = model_to_merge.merge_and_unload()
